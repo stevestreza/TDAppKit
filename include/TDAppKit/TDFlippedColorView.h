47d6241c91013fdf3dcdc556a6cbad0d920a1efd
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2010-10-20 15:32:49 +0000

    adding TDFlippedColorView
    git-svn-id: http://tdappkit.googlecode.com/svn/trunk@66 920da3f4-33dd-3378-2bda-26f8a5ac8392

--- include/TDAppKit/TDFlippedColorView.h
+++ include/TDAppKit/TDFlippedColorView.h
@@ -12,9 +12,9 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import <TDAppKit/TDFlippedView.h>
+#import <TDAppKit/TDColorView.h>
 
-@interface TDFlippedView : NSView {
+@interface TDFlippedColorView : TDColorView {
 
 }
 
--- src/TDFlippedColorView.m
+++ src/TDFlippedColorView.m
@@ -12,10 +12,12 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import <TDAppKit/TDFlippedView.h>
+#import <TDAppKit/TDFlippedColorView.h>
 
-@interface TDFlippedView : NSView {
+@implementation TDFlippedColorView
 
+- (BOOL)isFlipped {
+    return YES;
 }
 
 @end
