d056dab72e46f2902f440afc197956a0aa875421
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2010-10-20 04:55:11 +0000

    correct import for #import <TDAppKit/TDColorView.h>
    git-svn-id: http://tdappkit.googlecode.com/svn/trunk@65 920da3f4-33dd-3378-2bda-26f8a5ac8392

--- src/TDColorView.m
+++ src/TDColorView.m
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import "TDColorView.h"
+#import <TDAppKit/TDColorView.h>
 
 @implementation TDColorView
 
